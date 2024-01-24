e8b99dbb5d9f212cfbd1c3f1b2da728641c2a691
Drake Lewis <lewis5@kenyon.edu>
2024-01-24 09:17:32 -0500

    second version

--- HW2_Project.rb
+++ HW2_Project.rb
@@ -1,1 +1,14 @@
-print "Hello World"
+puts "Hello World"
+
+def gcd(a, b)
+    while a != b 
+        if a > b
+            a = a - b
+        else
+            b = b - a
+    return a
+        end
+    end
+end
+
+puts(gcd(52,8))
